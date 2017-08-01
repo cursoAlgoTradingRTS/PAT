76a6a64cd276c3b8b844ae374d7879fb2b36dda9
Fernando <fernandojcova@gmail.com>
2017-07-31 22:46:08 -0300

    Resolucion ejercicio 2

--- ejercicio1-2.py
+++ ejercicio1-2.py
@@ -8,6 +8,10 @@ d) Calcular el área de un rectángulo (alineado con los ejes x e y) dadas sus c
 e) Dados los catetos de un triángulo rectángulo, calcular su hipotenusa.
 '''
 
+#Debo importar la libreria Math que provee de funciones matematicas adicionales
+import math
+#Tomo pi de la libreria
+PI = math.pi
 
 def calcularPerimetroRectangulo(base, altura):
     return base * 2 + altura * 2
@@ -17,10 +21,49 @@ def calcularAreaRectangulo(base, altura):
     return base * altura
 
 
-def calcularAreaYPerimetro(base, altura):
+def calcularAreaYPerimetroRectangulo(base, altura):
     area = calcularAreaRectangulo(base, altura)
     perimetro = calcularPerimetroRectangulo(base, altura)
     print "El area es de " + str(area) + " y el perimetro es " + str(perimetro)
 
 
-calcularAreaYPerimetro(3, 5)
+calcularAreaYPerimetroRectangulo(3, 5)
+
+
+def calcularPerimetroCirculo(radio):
+    return radio*2.0*PI
+
+def calcularAreaCirculo(radio):
+    return PI*(radio**2)
+
+def calcularAreaYPerimetroCirculo(radio):
+    area = calcularAreaCirculo(radio)
+    perimetro = calcularPerimetroCirculo(radio)
+    print "El area es de " + str(area) + " y el perimetro es " + str(perimetro)
+
+calcularAreaYPerimetroCirculo(4)
+
+
+def calcularVolumenEsfera(radio):
+    volumen = (4/3)*PI*(radio**3)
+    print "El volumen de la esfera de radio " + str(radio) + " es de " + str(volumen)
+
+calcularVolumenEsfera(5)
+
+
+def calcularAreaRectanguloConCoordenadas(x1, y1, x2, y2):
+    lado1 = abs(x2-x1)
+    lado2 = abs(y2-y1)
+    area = lado1 * lado2
+    print "El area es de " + str(area)
+    return area
+
+calcularAreaRectanguloConCoordenadas(1,3,2,5)
+
+def calcularHipotenusa(cateto1, cateto2):
+    sumaCatetosCuadrado = cateto1**2 + cateto2**2
+    raiz = math.sqrt(sumaCatetosCuadrado)
+    print "La hipotenusa es " + str(raiz)
+    return  raiz
+
+calcularHipotenusa(3,4)
