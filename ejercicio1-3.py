417e718c0a230616022125db1cf128aa19cf1c70
Fernando <fernandojcova@gmail.com>
2017-08-01 10:11:22 -0300

    ejercicio 1-3.py

---
+++ ejercicio1-3.py
@@ -0,0 +1,14 @@
+'''
+Ejercicio 1.3. Mostrar el resultado de ejecutar estos bloques de codigo en el interprete de python:
+a)for i in range(5):
+... print(i * i)
+b)for i in range(2, 6):
+... print(i, 2 ** i)
+
+'''
+
+for i in range(5):
+    print(i * i)
+
+for i in range(2, 6):
+    print(i, 2 ** i)
