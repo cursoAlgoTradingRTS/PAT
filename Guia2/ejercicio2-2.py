e704fe60d3b3ce6b7922cd44759c30692f77fd0b
Fernando Cova <fernandojcova@gmail.com>
2017-08-16 23:32:40 -0300

    Ejercicio 2-2 resuelto

---
+++ Guia2/ejercicio2-2.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+'''Ejercicio 2.2. Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.
+Recordar que la fórmula para la conversión es: F = (9/5)C + 32'''
+
+
+def convertCelsiusToFarenheit(celsius):
+    value = (9 / 5) * celsius + 32
+    return value
+
+
+print "0 celsius son " + str(convertCelsiusToFarenheit(0)) + " Farenheit"
+print "10 celsius son " + str(convertCelsiusToFarenheit(10)) + " Farenheit"
+print "25 celsius son " + str(convertCelsiusToFarenheit(25)) + " Farenheit"
+print "100 celsius son " + str(convertCelsiusToFarenheit(100)) + " Farenheit"
